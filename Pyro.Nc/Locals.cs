7bb459e71f7de09a0a1f542a8ebce341c76a3c1e
Josip Mrdeža <forsakennydoseth@hotmail.com>
2022-08-08 23:02:58 +0200

    Added localisations for the rest of the commands added so far.

--- Pyro.Nc/Locals.cs
+++ Pyro.Nc/Locals.cs
@@ -1,5 +1,3 @@
-using UnityEngine.SceneManagement;
-
 namespace Pyro.Nc
 {
     public static class Locals
@@ -10,8 +8,15 @@ namespace Pyro.Nc
         public const string G03 = "Circular Move (Counter-Clockwise)";
         public const string CIP = "Circular Interpolation (Arc)";
         public const string G4 = "Dwell (Pause)";
+        public const string G60 = "Save Position";
+        public const string G61 = "Return to Saved Position";
         public const string G331 = "Drill";
         public const string G332 = "Retract (Drill)";
-        public const string M00 = "Unconditional stop (Pause)";
+        public const string M00 = "Unconditional Stop (Pause)";
+        public const string M01 = "Optional Stop (Pause)";
+        public const string M02 = "Spindle On (Clockwise)";
+        public const string M03 = "Spindle On (Counter-Clockwise)";
+        public const string M05 = "Spindle Stop";
+        public const string M06 = "Tool Change";
     }
 }
