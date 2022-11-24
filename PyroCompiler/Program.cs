453dde13a012e57c87a64c34ce728a19598205ab
Josip Mrdeža <forsakennydoseth@hotmail.com>
2022-11-24 16:53:22 +0100

    Created a new executable 'PyroCompiler' to get around the issue of unity using MONO to compile C# Code using CODEDOM.
    Signed-off-by: Josip Mrdeža <ForsakenNydoseth@hotmail.com>

---
+++ PyroCompiler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using Pyro.IO;
+using Pyro.IO.Builder;
+
+namespace PyroCompiler
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CompilerResults results = null;
+            Console.WriteLine("Compiling files...");
+            LocalRoaming roaming = LocalRoaming.OpenOrCreate("PyroNc\\Compiler\\References");
+            try
+            {
+                var assemblyName = args[0];
+                var pathToDirectory = args[1];
+                results = CodeImport.ImportTextLibrary(assemblyName, pathToDirectory,
+                                                       roaming.Files.Select(f => f.Value.FullName).ToArray(),
+                                                       args.Skip(2).ToArray());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                if (results.Errors.Count > 0)
+                {
+                    foreach (CompilerError error in results.Errors)
+                    {
+                        Console.WriteLine(error);
+                    }
+                }
+            }
+            Console.WriteLine("Finished!");
+        }
+    }
+}
